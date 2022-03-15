8853fa4aae133426588a078a0f401752cecba70b
erichvorster <erichvorster1@gmail.com>
2022-03-15 07:11:51 +0200

    3rd commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-
+/*
 const getCountryData = function (country) {
 
 
@@ -40,3 +40,60 @@ countriesContainer.style.opacity = 1;
 getCountryData('usa');
 
 getCountryData('portugal');
+*/
+
+//Function that holds our HTML - it will be reused
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+              <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+              <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+            </div>
+          </article>
+  
+  `
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+
+const getCountryAndNeighbour = function (country) {
+
+  // AJAX call for the first country
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send(); // Sending our request;
+  
+  request.addEventListener('load', function() {
+      const [data] = JSON.parse(this.responseText);//This is used to point to the event
+      console.log(data);
+
+    // Render country 1
+     renderCountry(data);
+
+    // Get neighbour country
+    const [neighbour] = data.borders;
+
+    //AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send(); // Sending our request;
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, "neighbour")
+    })
+  })
+  }
+  
+  
+  
+  getCountryAndNeighbour('ecuador')
+  
